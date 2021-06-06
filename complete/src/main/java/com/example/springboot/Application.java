85d2e6731d1cc196a770789629f23fcec66b949e
mcelam00 <mcelam00@estudiantes.unileon.es>
2021-06-06 04:40:53 +0200

    Código para el uso de contenedores en el servidor de aplicaciones tomcat con springboot (beans)

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -8,23 +8,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication //Aplicación springboot
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	//Creamos una componente. BEAN es una componente que se apoya en el contenedor que se instala en el apache tomcat
+
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) { //método que se convierte en un bean
+		return args -> { //una funcion lamda
 
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
+			String[] beanNames = ctx.getBeanDefinitionNames(); //Tiene las definiciones de los beans definidos en el contexto
+			Arrays.sort(beanNames); //se ordena el array
 			for (String beanName : beanNames) {
-				System.out.println(beanName);
+				System.out.println(beanName); //se visualiza
 			}
 
 		};
--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,11 +3,11 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@RestController//Separamos el Rest controller de la aplicacion. 
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping("/")//Respondemos a la peticion al directory root
+	public String index() { //se llama al método index
 		return "Greetings from Spring Boot!";
 	}
 
